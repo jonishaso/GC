b55069051a3f29e6f87451d456fb628a7a8c5175
illawarra allwa <illawarra.allwa@hotmail.com>
2016-08-10 10:24:19 +1000

    dd

---
+++ Client Final/MessageClientSide/mainForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace MessageClientSide
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.sendBtt = new System.Windows.Forms.Button();
+            this.hibBtt = new System.Windows.Forms.Button();
+            this.messageRichBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioCheckBox = new System.Windows.Forms.CheckBox();
+            this.mobileCheckBox = new System.Windows.Forms.CheckBox();
+            this.refreshBtt = new System.Windows.Forms.Button();
+            this.searchBtt = new System.Windows.Forms.Button();
+            this.searchComboBox = new System.Windows.Forms.ComboBox();
+            this.CleanBttn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            resources.ApplyResources(this.dataGridView, "dataGridView");
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            // 
+            // sendBtt
+            // 
+            resources.ApplyResources(this.sendBtt, "sendBtt");
+            this.sendBtt.Name = "sendBtt";
+            this.sendBtt.UseVisualStyleBackColor = true;
+            this.sendBtt.Click += new System.EventHandler(this.sendBtt_Click);
+            // 
+            // hibBtt
+            // 
+            resources.ApplyResources(this.hibBtt, "hibBtt");
+            this.hibBtt.Name = "hibBtt";
+            this.hibBtt.UseVisualStyleBackColor = true;
+            this.hibBtt.Click += new System.EventHandler(this.hibBtt_Click);
+            // 
+            // messageRichBox
+            // 
+            resources.ApplyResources(this.messageRichBox, "messageRichBox");
+            this.messageRichBox.Name = "messageRichBox";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // radioCheckBox
+            // 
+            resources.ApplyResources(this.radioCheckBox, "radioCheckBox");
+            this.radioCheckBox.Name = "radioCheckBox";
+            this.radioCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // mobileCheckBox
+            // 
+            resources.ApplyResources(this.mobileCheckBox, "mobileCheckBox");
+            this.mobileCheckBox.Name = "mobileCheckBox";
+            this.mobileCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // refreshBtt
+            // 
+            resources.ApplyResources(this.refreshBtt, "refreshBtt");
+            this.refreshBtt.Name = "refreshBtt";
+            this.refreshBtt.UseVisualStyleBackColor = true;
+            this.refreshBtt.Click += new System.EventHandler(this.refreshBtt_Click);
+            // 
+            // searchBtt
+            // 
+            resources.ApplyResources(this.searchBtt, "searchBtt");
+            this.searchBtt.Name = "searchBtt";
+            this.searchBtt.UseVisualStyleBackColor = true;
+            this.searchBtt.Click += new System.EventHandler(this.searchBtt_Click);
+            // 
+            // searchComboBox
+            // 
+            this.searchComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.searchComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.searchComboBox.BackColor = System.Drawing.Color.Gray;
+            this.searchComboBox.FormattingEnabled = true;
+            resources.ApplyResources(this.searchComboBox, "searchComboBox");
+            this.searchComboBox.Name = "searchComboBox";
+            // 
+            // CleanBttn
+            // 
+            resources.ApplyResources(this.CleanBttn, "CleanBttn");
+            this.CleanBttn.Name = "CleanBttn";
+            this.CleanBttn.UseVisualStyleBackColor = true;
+            this.CleanBttn.Click += new System.EventHandler(this.CleanBttn_Click);
+            // 
+            // mainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Controls.Add(this.CleanBttn);
+            this.Controls.Add(this.searchComboBox);
+            this.Controls.Add(this.searchBtt);
+            this.Controls.Add(this.refreshBtt);
+            this.Controls.Add(this.mobileCheckBox);
+            this.Controls.Add(this.radioCheckBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.messageRichBox);
+            this.Controls.Add(this.hibBtt);
+            this.Controls.Add(this.sendBtt);
+            this.Controls.Add(this.dataGridView);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "mainForm";
+            this.TransparencyKey = System.Drawing.SystemColors.ActiveBorder;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button sendBtt;
+        private System.Windows.Forms.Button hibBtt;
+        private System.Windows.Forms.RichTextBox messageRichBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox radioCheckBox;
+        private System.Windows.Forms.CheckBox mobileCheckBox;
+        private System.Windows.Forms.Button refreshBtt;
+        private System.Windows.Forms.Button searchBtt;
+        private System.Windows.Forms.ComboBox searchComboBox;
+        private System.Windows.Forms.Button CleanBttn;
+    }
+}
+
---
+++ Client Final/MessageClientSide/mainForm.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+
+
+namespace MessageClientSide
+{
+    public partial class mainForm : Form
+    {
+        private UdpClient udpReceiver;
+        private UdpClient udpSender;
+
+        private int receivePort = 11001;
+        private const int sendPort = 11002;
+        private const int serverListenPort = 11000;
+        private const string serverIPString = "10.108.96.44";  //gold coast server IP: 10.108.96.44 
+        private IPEndPoint serverAddress;
+        private DataTable local_db;
+        private event Action<DataTable> bindDB;
+
+        public mainForm()
+        {
+            string localIPString = Array.FindAll(Dns.GetHostEntry(Dns.GetHostName()).AddressList,
+                             a => a.AddressFamily == AddressFamily.InterNetwork)[0].ToString();
+            serverAddress = new IPEndPoint(IPAddress.Parse(serverIPString), serverListenPort);
+            udpReceiver = new UdpClient(new IPEndPoint(IPAddress.Parse(localIPString),receivePort));
+            udpSender = new UdpClient(new IPEndPoint(IPAddress.Parse(localIPString),sendPort));
+            bindDB += binding;          
+            InitializeComponent();
+        }
+        private void mainForm_Load(object sender, EventArgs e)
+        {   
+            byte[] bytes = Encoding.ASCII.GetBytes("init>");
+            udpSender.Send(bytes, bytes.Length, serverAddress);
+            udpReceiver.BeginReceive(new AsyncCallback(receiveCallBack), null);
+        }
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+        private void receiveCallBack(IAsyncResult obj)
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+            byte[] data = udpReceiver.EndReceive(obj, ref ep);
+            string jsonstring = Encoding.ASCII.GetString(data, 0, data.Length);
+            if (jsonstring == "HIMSending")
+            {
+                MessageBox.Show("The request has been processed by server....","Feedback");
+            }
+            else
+            {
+                try
+                {
+                    DataTable db = JsonConvert.DeserializeObject<DataTable>(jsonstring);
+                    db.CaseSensitive = false;
+                    bindDB(db);
+                }
+                catch (Exception e)
+                { MessageBox.Show(e.ToString(),"erro UDP package"); return; }
+            }
+            
+            udpReceiver.BeginReceive(new AsyncCallback(receiveCallBack), null);          
+        }
+        private void sendCallBack(IAsyncResult iar)
+        {
+            if (udpSender.EndSend(iar) > 0)
+                MessageBox.Show("Message(s) have been sent", "Information");
+        }
+
+        private void sendBtt_Click(object sender, EventArgs e)
+        {
+            if (messageRichBox.Text == "")
+            {
+                MessageBox.Show("you cannot send empty message");
+                return;
+            }
+            if (radioCheckBox.Checked && !mobileCheckBox.Checked)
+            {
+                foreach (DataGridViewRow row in dataGridView.SelectedRows)
+                {
+                    string udpPack = string.Format("radio>to:{0};msg:{1};", row.Cells[3].Value.ToString(), messageRichBox.Text);
+                    byte[] bytes = Encoding.ASCII.GetBytes(udpPack);
+                    udpSender.BeginSend(bytes, bytes.Length, serverAddress, new AsyncCallback(sendCallBack), null);
+                }
+            }
+            else if (!radioCheckBox.Checked && mobileCheckBox.Checked)
+            {
+                foreach (DataGridViewRow row in dataGridView.SelectedRows)
+                {
+                    string udpPack = string.Format("mobile>to:{0};msg:{1};", row.Cells[2].Value.ToString(), messageRichBox.Text);
+                    byte[] bytes = Encoding.ASCII.GetBytes(udpPack);
+                    //udpSender.Send(bytes, bytes.Length, serverAddress);
+                    udpSender.BeginSend(bytes, bytes.Length, serverAddress, new AsyncCallback(sendCallBack), null);
+                    Thread.Sleep(5000);
+                }
+            }
+            else if (radioCheckBox.Checked && mobileCheckBox.Checked)
+            {
+                foreach (DataGridViewRow row in dataGridView.SelectedRows)
+                {
+                    string udpPack = string.Format("radio>to:{0};msg:{1};", row.Cells[3].Value.ToString(), messageRichBox.Text);
+                    byte[] bytes = Encoding.ASCII.GetBytes(udpPack);
+                    udpSender.BeginSend(bytes, bytes.Length, serverAddress, new AsyncCallback(sendCallBack), null);
+
+                    udpPack = string.Format("mobile>to:{0};msg:{1};", row.Cells[2].Value.ToString(), messageRichBox.Text);
+                    bytes = Encoding.ASCII.GetBytes(udpPack);
+                    udpSender.BeginSend(bytes, bytes.Length, serverAddress, new AsyncCallback(sendCallBack), null);
+                    Thread.Sleep(5000);
+                }
+            }
+            else
+            {
+                MessageBox.Show("you have to select at lest one method of sending message");
+                return;
+            }
+
+        }
+        private void hibBtt_Click(object sender, EventArgs e)
+        {
+            if (messageRichBox.Text == "")
+            {
+                MessageBox.Show("you cannot send empty message");
+                return;
+            }
+            if (MessageBox.Show("Are you sure you want to broadcast to all HIM group ?", "Checking", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                string udpPack = string.Format("him>to:;msg:{0};", messageRichBox.Text);
+                byte[] bytes = Encoding.ASCII.GetBytes(udpPack);
+                udpSender.Send(bytes, bytes.Length, serverAddress);
+            }
+            else return;
+
+        }
+        private void refreshBtt_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes("init>");
+            udpSender.Send(bytes, bytes.Length, serverAddress);
+        }
+        private void searchBtt_Click(object sender, EventArgs e)
+        {
+            string searchValue = searchComboBox.Text;
+            foreach (DataGridViewRow row in dataGridView.SelectedRows)        
+                row.Selected = false;
+            DataView dv;
+            dv = new DataView(local_db, string.Format("name='{0}'",searchValue), null , DataViewRowState.CurrentRows);
+            dataGridView.DataSource = dv;
+        }
+        private void binding(DataTable db)
+        {
+            if (this.dataGridView.InvokeRequired)
+            {
+                Action<DataTable> b = new Action<DataTable>(binding);
+                this.Invoke(b, new object[] { db });
+            }
+            else
+            {
+                local_db = db;
+                local_db.CaseSensitive = false;
+                string[] names = db.Rows.Cast<DataRow>()
+                    .Select(row => row["name"].ToString())
+                    .ToArray();
+                searchComboBox.DataSource = names;
+                AutoCompleteStringCollection autoSource = new AutoCompleteStringCollection();
+                foreach (string i in names) autoSource.Add(i);
+                searchComboBox.AutoCompleteCustomSource = autoSource;
+                this.dataGridView.DataSource = db;
+            }
+        }
+        private void CleanBttn_Click(object sender, EventArgs e)
+        {
+            messageRichBox.Text = "";
+        }
+    }
+}
